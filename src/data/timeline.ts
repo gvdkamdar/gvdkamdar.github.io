fdc1711a99a9acfc8f86b9df519bb8c399a34cb4
gvdkamdar <67706180+gvdkamdar@users.noreply.github.com>
2025-03-01 18:50:39 -0500

    Update timeline.ts
    Updated timeline descriptions

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -15,9 +15,11 @@ export const timeline: TimelineItem[] = [
     location: "Stony Brook, US",
     period: "Jan 2024 – Present",
     achievements: [
-      "Designed ML workflow to extract human personas from aggregated socio-economic datasets under Prof. Klaus Mueller, reducing analysis time by 65%",
-      "Performed hierarchical clustering on US county-wide data on opioid deaths spanning 20 years, preserving 95% variance",
-      "Visualized 35,000+ datapoints in 14 clusters using dendrograms, enhancing persona accuracy by 25%"
+      "Designed ML workflow to extract human personas from aggregated socio-economic datasets under Prof. Klaus Mueller to provide actionable insights for policymakers and reducing analysis time by 65%.",
+      "Built a modular data pipeline to extract datapoints from pattern files generated from in-house AK Analyst tool.",
+      "Reduced noise by 40% using PCA, preserving 95% of variance in US county-wide data on opioid deaths spanning 20 years.",
+      "Performed hierarchical clustering with Ward’s linkage criterion and Euclidean distance, improving interpretability by 35%.",
+      "Visualized 35,000+ datapoints in 14 clusters using dendrograms. Enhanced persona accuracy by 25% through quantifying feature-specific percentage distribution, enabling targeted policymaking for opioid crisis management."
     ]
   },
   {
@@ -28,8 +30,8 @@ export const timeline: TimelineItem[] = [
     period: "Aug 2023 - May 2025",
     achievements: [
       "GPA: 3.4/4.0",
-      "Teaching Assistant: Programming Abstractions",
-      "Coursework: Machine Learning, Data Science, Database Systems, Visualization"
+      "Coursework: Machine Learning, Data Science, Database Systems, Visualization, Human-Computer Interaction, Principles of Programming Languages, Computing with Logic.",
+      "Teaching Assistant: Programming Abstractions."
     ]
   },
   {
@@ -39,10 +41,12 @@ export const timeline: TimelineItem[] = [
     location: "Mumbai, IN",
     period: "Sep 2022 – Aug 2023",
     achievements: [
-      "Led development of resource estimation tool for data migration tasks, automating RFP generation for 100+ users",
-      "Built and deployed XGBoost model using React.js, Flask, PostgreSQL, and Docker for scalable API endpoint",
-      "Demonstrated 60% reduction in RFP preparation time and 55% improved prediction accuracy",
-      "Collaborated on data migration pipelines for 10M+ records from SAP ECC to S/4HANA"
+      "Led the development of resource estimation tool for data migration tasks, automating generation of Request for Proposal, serving 100+ users & senior management.",
+      "Boosted prediction accuracy for underrepresented objects by 45% through KNN-based data imputation and correlation analysis between data objects across multiple business modules.",
+      "Optimized an XGBoost model via hyperparameter tuning with GridSearchCV, achieving R2 = 0.88 and RMSE = 2.7.",
+      "Built and deployed the prototype using React.js, Flask, PostgreSQL, and Docker, providing a scalable API endpoint.",
+      "Validated the tool prototype, demonstrating a potential 60% reduction in Request for Proposal preparation time and a 55% improvement in prediction accuracy compared to the Excel-based model, increasing client acquisition rates.",
+      "Collaborated with a cross-functional team to build data migration pipelines for over 10 million records from SAP ECC to S/4HANA system using SAP DMC, Business Application Studio and BODS for the largest retail client of the company"
     ]
   },
   {
@@ -64,9 +68,8 @@ export const timeline: TimelineItem[] = [
     location: "Mumbai, IN",
     period: "Jan 2021 – Apr 2021",
     achievements: [
-      "Enhanced financial reporting by automating MS SQL Server workflows with Alteryx",
-      "Developed Python-based ETL pipelines using SQLAlchemy & pyodbc with 20% reduction in processing time",
-      "Optimized data transformations and improved Power BI dashboards"
+      "Enhanced financial reporting by automating MS SQL Server workflows with Alteryx, optimizing data transformations, and improving Power BI dashboards, reducing manual processing time.",
+      "Developed Python-based ETL pipelines using SQLAlchemy & pyodbc demonstrating a 20% reduction in data processing time on a test dataset, showcasing potential for scalability and cost reduction.",
     ]
   },
   {
@@ -76,9 +79,7 @@ export const timeline: TimelineItem[] = [
     location: "Navi Mumbai, IN",
     period: "Dec 2019 – Jan 2020",
     achievements: [
-      "Built OCR web application for invoice management using Tesseract and OpenCV",
-      "Integrated React.js and Django for frontend and backend processing",
-      "Achieved 80% reduction in data processing time for invoice parsing"
+      "Built an OCR web application for invoice management using Tesseract and OpenCV library with a React.js and Django to parse unstructured company invoices into a PostgreSQL database with an 80% reduction in data processing time."
     ]
   },
   {
